a874774680be29a4efed9904da388b7d99047599
pjboer1 <boer.pim77@gmail.com>
2023-05-24 10:35:26 +0200

    Working on the charactercontroller version

--- GameLab/Assets/ThirdPersonMovement.cs
+++ GameLab/Assets/ThirdPersonMovement.cs
@@ -358,7 +358,7 @@ public class ThirdPersonMovement : MonoBehaviour
 
             if (direction.magnitude >= 0.1f)
             {
-                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + mainCamera.transform.eulerAngles.y;
+                float targetAngle = Mathf.Atan2(direction.x * 0.5f, direction.z) * Mathf.Rad2Deg + mainCamera.transform.eulerAngles.y;
                 float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
                 transform.rotation = Quaternion.Euler(0f, angle, 0f);
                 Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
